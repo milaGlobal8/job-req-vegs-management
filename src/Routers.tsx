ac8d3a5d49f66c7f87691990ec15aaef48ed7574
ShunIshizeki <shun2000c9@outlook.jp>
2023-10-30 12:55:32 +0900

    ルート設定。

--- src/Routers.tsx
+++ src/Routers.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Detail from "./pages/detail/Detail";
+import Home from "./pages/home/Home";
 
 export default function Routers() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" />
-          <Route path="/detail" />
+          <Route path="/" element={<Home />} />
+          <Route path="/detail" element={<Detail />} />
         </Routes>
       </BrowserRouter>
     </>
