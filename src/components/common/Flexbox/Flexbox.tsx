81dc862d75414bc5d9d48e3c2ae3ded09c3b8f17
ShunIshizeki <shun2000c9@outlook.jp>
2023-10-31 15:33:00 +0900

    propsに新しいpropsを加えた。

--- src/components/common/Flexbox/Flexbox.tsx
+++ src/components/common/Flexbox/Flexbox.tsx
@@ -3,8 +3,27 @@ import styles from "./Flexbox.module.scss";
 import { FlexboxProps } from "./Flexbox.types";
 
 const Flexbox = (props: FlexboxProps) => {
-  const { children, className } = props;
-  return <div className={classNames(styles.flex, className)}>{children}</div>;
+  const {
+    children,
+    className,
+    justifyContent,
+    alignItems,
+    flexDirection,
+    gap,
+  } = props;
+  return (
+    <div
+      className={classNames(styles.flex, className)}
+      style={{
+        justifyContent: justifyContent,
+        alignItems: alignItems,
+        flexDirection: flexDirection,
+        gap: gap,
+      }}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Flexbox;
--- src/components/common/Flexbox/Flexbox.types.ts
+++ src/components/common/Flexbox/Flexbox.types.ts
@@ -3,4 +3,13 @@ import { ReactNode } from "react";
 export type FlexboxProps = {
   children?: ReactNode;
   className?: string;
+  justifyContent?:
+    | "center"
+    | "space-between"
+    | "space-around"
+    | "space-evenly"
+    | "end";
+  alignItems?: "start" | "center" | "end";
+  flexDirection?: "column";
+  gap?: number;
 };
