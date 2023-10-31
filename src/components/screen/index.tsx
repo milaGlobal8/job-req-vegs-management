06695fe6ced576378e13f8ca43e9b0b566966297
ShunIshizeki <shun2000c9@outlook.jp>
2023-10-31 15:38:14 +0900

    エクスポートに追加 。

--- src/components/screen/index.tsx
+++ src/components/screen/index.tsx
@@ -1,3 +1,5 @@
 export { default as Header } from "./Header/Header";
 
 export { default as PageTitle } from "./PageTitle/PageTitle";
+
+export { default as UpdatedAt } from "./UpdatedAt/UpdatedAt";
