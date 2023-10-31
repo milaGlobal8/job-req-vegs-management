cf819c520549389df8a08689eb44c71399ca3911
ShunIshizeki <shun2000c9@outlook.jp>
2023-10-31 18:35:34 +0900

    cardを一括エクスポートファイルに記入。

--- src/components/screen/index.tsx
+++ src/components/screen/index.tsx
@@ -3,3 +3,5 @@ export { default as Header } from "./Header/Header";
 export { default as PageTitle } from "./PageTitle/PageTitle";
 
 export { default as UpdatedAt } from "./UpdatedAt/UpdatedAt";
+
+export { default as Card } from "./Card/Card";
