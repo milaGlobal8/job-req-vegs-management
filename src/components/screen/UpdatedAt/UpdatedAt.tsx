2886e4a9ace000fa89bef6dae46f9ea8c749e160
ShunIshizeki <shun2000c9@outlook.jp>
2023-10-31 15:48:07 +0900

    updatedAtとりあえず完成。

--- src/components/screen/UpdatedAt/UpdatedAt.tsx
+++ src/components/screen/UpdatedAt/UpdatedAt.tsx
@@ -1,3 +1,17 @@
-const UpdatedAt = () => {};
+import UpdateIcon from "@mui/icons-material/Update";
+
+import { Flexbox, Typography } from "../../common";
+import { UpdatedAtProps } from "./UpdatedAt.types";
+
+const UpdatedAt = (props: UpdatedAtProps) => {
+  const { children } = props;
+
+  return (
+    <Flexbox justifyContent="center" alignItems="center" gap={10}>
+      <UpdateIcon />
+      <Typography>{children}</Typography>
+    </Flexbox>
+  );
+};
 
 export default UpdatedAt;
