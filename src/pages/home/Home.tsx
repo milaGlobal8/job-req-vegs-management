7b88ba5bfaba655cfc3ac10ce7174ac2abf972b1
ShunIshizeki <shun2000c9@outlook.jp>
2023-10-31 15:47:45 +0900

    updatedAtコンポーネントを追加。

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,11 @@
 import { Flexbox } from "../../components/common";
-import { PageTitle } from "../../components/screen";
+import { PageTitle, UpdatedAt } from "../../components/screen";
 
 export default function Home() {
   return (
     <Flexbox className="home_page" flexDirection="column">
       <PageTitle>Home</PageTitle>
+      <UpdatedAt>2023/10/31 15:46</UpdatedAt>
     </Flexbox>
   );
 }
