ff3adf05a4b079dd1faac78b710d35f2e5dc5030
ShunIshizeki <shun2000c9@outlook.jp>
2023-11-01 14:42:27 +0900

    Homeをpagesで一括エクスポートできるようにした。

--- src/Routers.tsx
+++ src/Routers.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Header } from "./components/screen";
-import Home from "./pages/home/Home";
+import { Home } from "./pages";
 
 export default function Routers() {
   return (
