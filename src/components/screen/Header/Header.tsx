39a5d54da8f409754d173460d72001e28c1d1454
ShunIshizeki <shun2000c9@outlook.jp>
2023-11-01 12:02:29 +0900

    ロゴを真ん中に移動。

--- src/components/screen/Header/Header.tsx
+++ src/components/screen/Header/Header.tsx
@@ -1,10 +1,10 @@
-import Logo from "../../../assets/sample-logo.jpg";
+import Logo from "../../../assets/logo.jpg";
 import { Flexbox, Image } from "../../common";
 import styles from "./Header.module.scss";
 
 const Header = () => {
   return (
-    <Flexbox className={styles.header}>
+    <Flexbox className={styles.header} justifyContent="center">
       <Image alt="ヘッダーロゴ" className={styles.img_container} src={Logo} />
     </Flexbox>
   );
