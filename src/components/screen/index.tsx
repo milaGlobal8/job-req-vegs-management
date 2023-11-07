f1311f5c55943d83435d88ca3986aa0c2fe3cbc8
ShunIshizeki <shun2000c9@outlook.jp>
2023-11-08 07:36:53 +0900

    一括エクスポートファイルにまとめた。

--- src/components/screen/index.tsx
+++ src/components/screen/index.tsx
@@ -1,7 +1,4 @@
-export { default as Header } from "./Header/Header";
-
-export { default as PageTitle } from "./PageTitle/PageTitle";
-
-export { default as UpdatedAt } from "./UpdatedAt/UpdatedAt";
-
 export { default as Card } from "./Card/Card";
+export { default as Header } from "./Header/Header";
+export { default as PageTitle } from "./PageTitle/PageTitle";
+export { default as UpdatedAt } from "./UpdatedAt/UpdatedAt";
