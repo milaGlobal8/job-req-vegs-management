14e74a6ee6e7e3d3a891ceac43e12cd671ab04a8
ShunIshizeki <shun2000c9@outlook.jp>
2023-11-09 09:34:25 +0900

    margin-bottomを削除した。

--- src/components/screen/Version/Version.tsx
+++ src/components/screen/Version/Version.tsx
@@ -1,16 +1,11 @@
 import { Flexbox } from "../../common";
-import styles from "./Version.module.scss";
 import { VersionProps } from "./Version.types";
 
 const Version = (props: VersionProps) => {
   const { children } = props;
 
   return (
-    <Flexbox
-      className={styles.version}
-      justifyContent="center"
-      alignItems="center"
-    >
+    <Flexbox className="version" justifyContent="center" alignItems="center">
       {children}
     </Flexbox>
   );
