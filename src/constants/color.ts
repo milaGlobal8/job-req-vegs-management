4a561f929db4e0456a8d630ac225ae6e8f86eb11
ShunIshizeki <shun2000c9@outlook.jp>
2023-11-09 16:04:07 +0900

    色追加。

--- src/constants/color.ts
+++ src/constants/color.ts
@@ -5,12 +5,15 @@ export const COLOR = {
   W2: "w2",
   RED1: "red1",
   BLUE1: "blue1",
+  BLUE2: "blue2",
   GREEN1: "green1",
   GREEN2: "green2",
   ORANGE1: "orange1",
   ORANGE2: "orange2",
   PINK1: "pink1",
   PINK2: "pink2",
+  GOLD1: "gold1",
+  SILVER1: "silver1",
 } as const;
 
 // as const : const assertionという機能でObjや値をreadonly(再代入不可)にする機能
