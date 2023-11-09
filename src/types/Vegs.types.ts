ac465543a3c7d800d60b4293df2f7cdc91d656d8
ShunIshizeki <shun2000c9@outlook.jp>
2023-11-09 11:00:47 +0900

    データの型を変更。

--- src/types/Vegs.types.ts
+++ src/types/Vegs.types.ts
@@ -1,11 +1,14 @@
 import { Timestamp } from "firebase/firestore";
 
 export type Vegs = {
-  alt: string;
+  alt?: string;
+  category?: "A" | "B";
   createdAt?: Timestamp;
-  desc: string;
-  image: string;
-  name: string;
-  stock: number;
-  updatedAt: Timestamp;
+  desc?: string;
+  image?: string;
+  name?: string;
+  price?: number;
+  shape?: "袋" | "箱" | "g";
+  stock?: number;
+  updatedAt?: Timestamp;
 };
