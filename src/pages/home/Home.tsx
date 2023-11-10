64f890f66eec4e55129c9413f3befbfca0807d82
ShunIshizeki <shun2000c9@outlook.jp>
2023-11-10 13:44:08 +0900

    リファクタリング。

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -21,24 +21,47 @@ export default function Home() {
   return (
     <Flexbox className={styles.home_page} flexDirection="column" gap={20}>
       <UpdatedAt>
-        {vegs.length !== 0
-          ? dayjs(vegs[0].updatedAt.toDate()).format("YYYY/MM/DD HH:mm")
-          : "更新中..."}
+        {vegs.length === 0
+          ? "更新中..."
+          : dayjs(vegs[0].updatedAt.toDate()).format("YYYY/MM/DD HH:mm")}
       </UpdatedAt>
       <Flexbox justifyContent="center" flexWrap="wrap" gap={30} margin={45}>
-        {vegs.length !== 0 ? (
+        {vegs.length === 0 ? (
+          <Loading loadingMsg="データ取得中..." />
+        ) : (
           vegs.map((veg) => (
             <Card
               key={crypto.randomUUID()}
               alt={veg.alt}
-              src={veg.image}
-              name={veg.name}
-              stock={veg.stock}
               desc={veg.desc}
+              image={veg.image}
+              name={veg.name}
+              // A品
+              // 袋
+              aBagQty={veg.aBagQty}
+              aBagStock={veg.aBagStock}
+              aBagPrice={veg.aBagPrice}
+              // 箱
+              aBoxQty={veg.aBoxQty}
+              aBoxStock={veg.aBoxStock}
+              aBoxPrice={veg.aBoxPrice}
+              // g
+              aGramStock={veg.aGramStock}
+              aGramPrice={veg.aGramPrice}
+              // B品
+              // 袋
+              bBagQty={veg.bBagQty}
+              bBagStock={veg.bBagStock}
+              bBagPrice={veg.bBagPrice}
+              // 箱
+              bBoxQty={veg.bBoxQty}
+              bBoxStock={veg.bBoxStock}
+              bBoxPrice={veg.bBoxPrice}
+              // g
+              bGramStock={veg.bGramStock}
+              bGramPrice={veg.bGramPrice}
             />
           ))
-        ) : (
-          <Loading loadingMsg="データ取得中..." />
         )}
       </Flexbox>
       {/* アプリバージョン情報 */}
