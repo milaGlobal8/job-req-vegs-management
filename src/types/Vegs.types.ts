b6b9a2ff61c8f4e86880c08b563eecaf7ee5bca6
ShunIshizeki <shun2000c9@outlook.jp>
2023-11-10 13:44:21 +0900

    データ構造変更による修正。

--- src/types/Vegs.types.ts
+++ src/types/Vegs.types.ts
@@ -1,14 +1,35 @@
 import { Timestamp } from "firebase/firestore";
 
 export type Vegs = {
+  // A品
+  // 袋
+  aBagQty: number;
+  aBagStock: number;
+  aBagPrice: number;
+  // 箱
+  aBoxQty: number;
+  aBoxStock: number;
+  aBoxPrice: number;
+  // g
+  aGramStock: number;
+  aGramPrice: number;
+  // B品
+  // 袋
+  bBagQty: number;
+  bBagStock: number;
+  bBagPrice: number;
+  // 箱
+  bBoxQty: number;
+  bBoxStock: number;
+  bBoxPrice: number;
+  // g
+  bGramStock: number;
+  bGramPrice: number;
+  // 他のフィールド
   alt?: string;
-  category?: "A" | "B";
   createdAt?: Timestamp;
   desc?: string;
   image?: string;
   name?: string;
-  price?: number;
-  shape?: "袋" | "箱" | "g";
-  stock?: number;
   updatedAt: Timestamp;
 };
