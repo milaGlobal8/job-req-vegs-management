ebc5a02c98b29414c0c7ebd172dbf5c4cd3b2968
ShunIshizeki <shun2000c9@outlook.jp>
2023-11-30 11:39:30 +0900

    絶対パス修正

--- src/components/block/Header/Header.tsx
+++ src/components/block/Header/Header.tsx
@@ -1,4 +1,4 @@
-import Logo from "../../../assets/logo/logo.jpg";
+import Logo from "assets/logo/logo.jpg";
 import { Flexbox, Image } from "../../atoms";
 import styles from "./Header.module.scss";
 
