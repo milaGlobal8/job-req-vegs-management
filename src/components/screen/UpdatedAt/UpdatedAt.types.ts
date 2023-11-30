631bbf7d3d3fda4e441c5a5046cb0d13a441cffc
ShunIshizeki <shun2000c9@outlook.jp>
2023-11-30 11:23:56 +0900

    ディレクトリ名をscreenからblockに名前を変更

--- src/components/screen/UpdatedAt/UpdatedAt.types.ts
+++
@@ -1,5 +0,0 @@
-import { ReactNode } from "react";
-
-export type UpdatedAtProps = {
-  children?: ReactNode;
-};
--- src/components/screen/index.tsx
+++
@@ -1,8 +0,0 @@
-export { default as Card } from "./Card/Card";
-export { default as Header } from "./Header/Header";
-export { default as Loading } from "./Loading/Loading";
-export { default as PageTitle } from "./PageTitle/PageTitle";
-export { default as Section } from "./Section/Section";
-export { default as Tag } from "./Tag/Tag";
-export { default as UpdatedAt } from "./UpdatedAt/UpdatedAt";
-export { default as Version } from "./Version/Version";
